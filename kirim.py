ae8f8b002603df05efa3011154493dce2d5b1b43
rimbafrida <53026977+rimbafrida@users.noreply.github.com>
2020-07-26 19:06:27 +0700

    Create kirim.py

---
+++ kirim.py
@@ -0,0 +1,32 @@
+import socket
+import json
+from cryptography.fernet import Fernet
+
+
+
+listensocket = socket.socket()
+port=8088
+ip =''
+listensocket.bind(('',port))
+listensocket.listen(5)
+print ("menunggu koneksi")
+clientsocket, client_address = listensocket.accept()
+#watch=Timer()
+#watch.start()
+running = True
+while running:
+        file = '/home/pausjumpstyle/received_file.json'
+        s = open(file).read()
+        data = str(json.loads(s))
+        print (data)
+        file = open('key.key', 'rb')
+        key = file.read() # The key will be type bytes
+        file.close()
+
+        f2=Fernet(key)
+        decrypted = f2.encrypt(data)
+        clientsocket.send(decrypted.encode())
+
+        break
+
+clientsocket.close()
